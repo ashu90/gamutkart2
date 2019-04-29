13f41002a1f837e0051b3b5bb1a200accfd99c59
Ashutosh <ashutech.kumar@gmail.com>
2019-04-29 15:09:51 +0530

    modified the multi_deploy.sh file as per requirement

--- multi_deploy.sh
+++ multi_deploy.sh
@@ -5,10 +5,10 @@ for i in `cat multi-deploy-IPs.txt`
 #
 	echo "Deploying war file into $i QA server.."
 	sleep 3
-	sshpass -p "ashu@123" scp target/gamutkart.war root@$i:/opt/software/apache-tomcat-8.5.39/webapps/
+	sshpass -p "$MYPASSWORD" scp target/gamutkart.war root@$i:/opt/software/apache-tomcat-8.5.39/webapps/
 #
 	echo "Starting tomcat server in $i QA server.."
-	sshpass -p "ashu@123" ssh root@$i "JAVA_HOME=/opt/software/jdk1.8.0_201" "/opt/software/apache-tomcat-8.5.39/bin/startup.sh"
+	sshpass -p "$MYPASSWORD" ssh root@$i "JAVA_HOME=/opt/software/jdk1.8.0_201" "/opt/software/apache-tomcat-8.5.39/bin/startup.sh"
 #
 	done
 	echo "Deployment is succussful!"
